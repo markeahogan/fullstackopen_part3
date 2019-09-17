68fb28ef9546fc6ffd23dd6c9e212fa38c72d191
Mark Hogan <mark@hiddenltd.com>
2019-09-17 13:34:31 +0100

    3.12: Command-line database
    Add mongoose and mongo.js with commandline features

---
+++ phonebook_backend/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+if ( process.argv.length<3 ) {
+    console.log('give password as argument');
+    process.exit(1);
+}
+
+const password = process.argv[2];
+const url = `mongodb+srv://fullstack:${password}@cluster0-s2ye6.mongodb.net/test?retryWrites=true&w=majority`;
+
+mongoose.connect(url, { useNewUrlParser: true,  useUnifiedTopology: true });
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+const addPerson = (name, number) => {   
+    
+    const person = new Person({name, number});
+
+    person.save().then(response => {
+        console.log(`added ${person.name} number ${person.number} to phonebook`);
+        mongoose.connection.close();
+    });
+};
+
+const listPersons = () => {
+
+    Person.find({}).then(result => {
+        result.forEach(person => console.log(`${person.name} ${person.number}`));        
+        mongoose.connection.close();
+    });
+};
+
+if (process.argv.length >= 5){
+    addPerson(process.argv[3], process.argv[4]);
+}else{
+    listPersons();
+}

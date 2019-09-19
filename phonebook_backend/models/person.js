30441f49bbed5e3249d4bc365e9105f92d27b105
Mark Hogan <mark@hiddenltd.com>
2019-09-19 18:09:42 +0100

    Add toJSON method to person schema

--- phonebook_backend/models/person.js
+++ phonebook_backend/models/person.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const url = process.env.MONGODB_URL;
+const url = process.env.MONGODB_URI;
 
 console.log("Connecting to mongodb")
 
-mongoose.connect(url, { useNewUrlParser: true,  useUnifiedTopology: true })
+mongoose.connect(url, { useNewUrlParser: true,  useUnifiedTopology: true, useFindAndModify: false  })
     .then(() => {
         console.log("Connected");
     })
@@ -16,6 +16,14 @@ const personSchema = new mongoose.Schema({
     name: String,
     number: String
 });
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString();
+      delete returnedObject._id;
+      delete returnedObject.__v;
+    }
+  })
 
 const Person = mongoose.model('Person', personSchema);
 
